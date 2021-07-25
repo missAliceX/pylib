d8741371c5cf81e847acd3f6b59bc30b45b54123
Alice Xia <miss.alice.xia@gmail.com>
2021-07-25 11:52:42 -0700

    add lib

---
+++ src/postgres/__init__.py
@@ -0,0 +1,42 @@
+from psycopg2.pool import SimpleConnectionPool
+from contextlib import contextmanager
+import os
+
+
+class Postgres:
+    pool = None
+
+    @classmethod
+    def connect(cls, cfg):
+        cls.pool = SimpleConnectionPool(
+            1, 20,
+            host=cfg["postgres_host"],
+            database=cfg["postgres_db"],
+            user=cfg["postgres_user"],
+            password=cfg["postgres_password"]
+        )
+
+    @classmethod
+    @contextmanager
+    def repo(cls, commit=True):
+        conn = cls.pool.getconn()
+        try:
+            yield conn.cursor()
+            if commit:
+                conn.commit()
+        except Exception:
+            conn.rollback()
+            raise
+        finally:
+            cls.pool.putconn(conn)
+
+    @classmethod
+    def migrate(cls, mode):
+        with cls.repo() as cursor:
+            for root, dirs, files in os.walk('migrations'):
+                for file in files:
+                    if file.endswith(f'.{mode}.sql'):
+                        cursor.execute(open(os.path.join(root, file)).read())
+
+            
+                
