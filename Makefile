cfd73191c332c219a5d3e9bc09296b6eba0f6d2f
Alice Xia <miss.alice.xia@gmail.com>
2021-07-25 16:21:31 -0700

    use setup.py

--- Makefile
+++ Makefile
@@ -1,3 +1,8 @@
+install:
+	pip-upgrade
+	pip install -U -r requirements.txt
+	pip install -U -r requirements.dev.txt
+
 generate:
 	python -m grpc_tools.protoc -I proto \
 		--python_out=src/pylib/proto --grpc_python_out=src/pylib/proto \
---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.readlines()
+
+setup(name='pylib',
+      version='0.0.1',
+      description='All your Python services need',
+      author='Alice Xia',
+      author_email='miss.alice.xia@gmail.com',
+      url='https://github.com/missAliceXia/pylib/',
+      packages=find_packages(where='src'),
+      install_requires=requirements
+      )
