77fe613bccc1c777adbd130d309365eca3b0d468
Alice Xia <miss.alice.xia@gmail.com>
2021-07-25 12:15:03 -0700

    add migrations to repo

---
+++ src/pylib/postgres/migrations/0_create_tables.up.sql
@@ -0,0 +1,20 @@
+CREATE TABLE IF NOT EXISTS projects (
+    project_id serial PRIMARY KEY,
+    project_name VARCHAR ( 80 ) UNIQUE NOT NULL,
+    tagline VARCHAR ( 255 ) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS threads (
+    thread_id serial PRIMARY KEY,
+    project_id integer REFERENCES projects NOT NULL,
+    main_thread_id integer REFERENCES threads ON DELETE CASCADE,
+    thread_type VARCHAR ( 12 ) NOT NULL,
+    message text NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS tags (
+    tag_id serial PRIMARY KEY,
+    project_id integer REFERENCES projects,
+    thread_id integer REFERENCES threads,
+    tag VARCHAR( 50 ) NOT NULL
+);
