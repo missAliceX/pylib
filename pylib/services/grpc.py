1c47cb47e38055ea2beadb43d07456b78dffbd12
Alice Xia <miss.alice.xia@gmail.com>
2021-07-25 21:51:26 -0700

    Update grpc.py

--- pylib/services/grpc.py
+++ pylib/services/grpc.py
@@ -2,11 +2,11 @@ from concurrent.futures import ThreadPoolExecutor
 import grpc
 
 class GRPCService:
-    def __init__(self, register, ServiceClass):
+    def __init__(self, ServiceClass, register):
         self.server = grpc.server(ThreadPoolExecutor(max_workers=10))
         register(ServiceClass(), self.server)
 
-    def start(self, register, ServiceClass):
+    def start(self):
         self.server.add_insecure_port("[::]:9090")
         self.server.start()
         self.server.wait_for_termination()
