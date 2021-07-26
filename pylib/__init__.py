bf2b619884575fe550be4c2490c3b5bde7d1d9e2
Alice Xia <miss.alice.xia@gmail.com>
2021-07-25 22:26:23 -0700

    remove duplicate

--- pylib/__init__.py
+++ pylib/__init__.py
@@ -1,6 +1,8 @@
 import logging
 import os
 
+logging.getLogger("sanic.access").propagate = False
+logging.getLogger("sanic.root").propagate = False
 logging.basicConfig(
     format='[%(asctime)s] [%(process)d] [%(levelname)s] %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S %z',
