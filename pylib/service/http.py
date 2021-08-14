aeed901802a87dbc781845ba710d351b184ff46f
Alice Xia <miss.alice.xia@gmail.com>
2021-08-13 22:10:49 -0700

    Update http.py

--- pylib/service/http.py
+++ pylib/service/http.py
@@ -4,11 +4,11 @@ from sanic import Sanic
 class HTTPService:
     def __init__(self, name="demo", cfg={}):
         # Creates a Sanic HTTP server
-        self.app = Sanic(name, port=8080)
+        self.app = Sanic(name)
 
         # Adds the HTTP handlers and routes to the Sanic server
         self.register()
 
     def start(self):
         # Starts the server
-        self.app.run()
+        self.app.run(port=8080)
